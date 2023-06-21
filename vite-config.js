e422d4b5c094cac490634353a92776fa17ee8b58
Jorge Saa <piojo.sistematico@gmail.com>
2023-06-20 20:22:47 -0400

    new directory

--- vite-config.js
+++ vite-config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/fem-huddle-landing-page-with-single-introductory-section-master/",
+  base: "/fem-huddle-landing-page-with-single-introductory/",
 });
